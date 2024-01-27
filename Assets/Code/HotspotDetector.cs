03d2230c150180bd5635be95a01422f3a15ebe77
Adam Englishby <adam@englishby.ie>
2024-01-27 11:49:17 +0000

    fix(hotspots): refactor & correctly check for interaction being enabled

--- Assets/Code/HotspotDetector.cs
+++ Assets/Code/HotspotDetector.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider), typeof(Rigidbody))]
 public class HotspotDetector : MonoBehaviour
 {
+    [SerializeField] private Player player;
     [SerializeField] private Color edgeHighlightColour;
 
     private readonly List<Hotspot> _hotspots = new();
@@ -20,34 +21,36 @@ public class HotspotDetector : MonoBehaviour
             var distance2 = Vector3.Distance(h2.transform.position, ourPosition);
             return Mathf.RoundToInt(distance1 * 100 - distance2 * 100);
         });
-        _hotspots.First().Interact();
+        var h = _hotspots.First();
+        SetEdgeColour(Color.black, h);
+        h.Interact();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!player.InteractionEnabled) return;
         if (!other.TryGetComponent(out Hotspot hotspot)) return;
-        foreach (var r in hotspot.GetComponentsInChildren<Renderer>())
-        {
-            foreach (var mat in r.materials)
-            {
-                mat.SetColor(OutlineColor, edgeHighlightColour);
-            }
-        }
-
+        SetEdgeColour(edgeHighlightColour, hotspot);
         _hotspots.Add(hotspot);
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (!player.InteractionEnabled) return;
         if (!other.TryGetComponent(out Hotspot hotspot)) return;
+        SetEdgeColour(Color.black, hotspot);
+        _hotspots.Remove(hotspot);
+    }
+
+    private static void SetEdgeColour(Color color, Component hotspot)
+    {
         foreach (var r in hotspot.GetComponentsInChildren<Renderer>())
         {
             foreach (var mat in r.materials)
             {
-                mat.SetColor(OutlineColor, Color.black);
+                mat.SetColor(OutlineColor, color);
             }
         }
-
-        _hotspots.Remove(hotspot);
     }
+    
 }
