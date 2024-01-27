22dba8d9d8aa266776121512b08fb3f7cbff3e7f
Adam Englishby <adam@englishby.ie>
2024-01-27 10:50:51 +0000

    feat(player): add character controller + assets

---
+++ Assets/Player.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Morrigan.Scripts.Character.LocomotionSystem
+{
+    public class Player : LocomotionDriver
+    {
+        private Vector3 _inputDirection;
+        
+        [Header("Player Locomotion Parameters")]
+        [SerializeField] private float speed = 7.5f;
+        [SerializeField] private float turnSpeed = 20f;
+        [SerializeField] private float acceleration = 10f;
+        [SerializeField] private float deceleration = 5f;
+
+        private float _actualSpeed = 0f;
+
+        protected override float Speed
+        {
+            get => _actualSpeed;
+            set => speed = value;
+        }
+
+        protected override float TurnSpeed
+        {
+            get => turnSpeed;
+            set => turnSpeed = value;
+        }
+
+        private void OnEnable()
+        {
+            base.Init();
+        }
+
+        private new void Update()
+        {
+            base.Update();
+
+            if (Camera.main is null) return;
+
+            var dir = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * _inputDirection;
+            base.InputDirection = new Vector3(dir.x, 0, dir.z);
+            
+            if (base.InputDirection.magnitude > 1)
+            {
+                base.InputDirection.Normalize();
+            }
+
+            var input = base.InputDirection.magnitude > 0;
+            _actualSpeed = Mathf.Lerp(_actualSpeed, input ? speed : 0, (input ? acceleration  : deceleration) * Time.deltaTime);
+        }
+        
+        protected override Vector3 GetGravity()
+        {
+            return new Vector3(0, -9.81f, 0);
+        }
+        
+        public void OnMove(InputValue value)
+        {
+            if (base.MoveDirectionOverridden) return;
+            
+            var input = value.Get<Vector2>();
+            
+            // only normalise input if magnitude is over 1
+            // this allows for more fine-grained analog input
+            if (input.magnitude > 1)
+            {
+                input.Normalize();
+            }
+
+            _inputDirection = new Vector3(input.x, 0, input.y);
+        }
+
+    }
+}
